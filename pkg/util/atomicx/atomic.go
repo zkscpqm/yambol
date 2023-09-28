4ad846d865304358f950c047265f575833ea302e
zkscpqm <zkscpqm@daum.net>
2023-09-28 15:29:33 +0300

    Implemented test workflows for PRs and build workflows for master pushes with updated version. Removed CGO implementation for MaxSwap

--- pkg/util/atomicx/atomic.go
+++ pkg/util/atomicx/atomic.go
@@ -1,13 +1,18 @@
 package atomicx
 
-/*
-#include "atomic.h"
-*/
-import "C"
+import "sync/atomic"
 
 func MaxSwap64(tgt *uint64, val uint64) {
 	if tgt == nil {
 		return
 	}
-	C.maxSwap64((*C.uint64_t)(tgt), C.uint64_t(val))
+	for {
+		old := atomic.LoadUint64(tgt)
+		if val <= old {
+			break
+		}
+		if atomic.CompareAndSwapUint64(tgt, old, val) {
+			break // The value was successfully swapped.
+		}
+	}
 }
